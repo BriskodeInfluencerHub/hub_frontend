0ec54641f6d1152bdaab1e0e8076980e765ee9af
Soni <sonisingh2004@gmail.com>
2026-06-27 10:42:07 +0530

    chore: revert api base URL to local development environment

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  // baseURL: 'http://localhost:5001/api',
-  baseURL: 'https://hub-backend-3db8.onrender.com/api',
+  baseURL: 'http://localhost:5001/api',
+  // baseURL: 'https://hub-backend-3db8.onrender.com/api',
   headers: {
     'Content-Type': 'application/json',
   },
